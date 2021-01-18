c178b50700e6c267307201d7d0f9cf40617dbcc4
YuriyMokhov HomePC <mokhov86@yandex.ru>
2021-01-18 12:52:46 +0300

    1) Summary all 2) Added atributes for client validation

--- MobileStoreWithSQLite/Models/Domain/Person.cs
+++ MobileStoreWithSQLite/Models/Domain/Person.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileStoreWithSQLite.Utils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,14 +7,22 @@ using System.Threading.Tasks;
 
 namespace MobileStoreWithSQLite.Models.Domain
 {
+    [PasswordNotEqualName]
     public class Person
     {
-        [Required]
+        [Required(ErrorMessage = "Вводи имя сука!")]
+        [Display(Name = "Имя")]
         public string Name { get; set; }
         [Required]
+        [EmailAddress]
+        [Display(Name = "Почта")]
         public string Email { get; set; }
         [Required]
+        [Display(Name = "Пароль сука")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        [Range(8,118, ErrorMessage = "Выберите адекватный возраст! сука")]
+        [Display(Name = "Леток те скока сука?!")]
         public int Age { get; set; }
     }
 }
---
+++ MobileStoreWithSQLite/Utils/PasswordNotEqualNameAttribute.cs
@@ -0,0 +1,25 @@
+﻿using MobileStoreWithSQLite.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileStoreWithSQLite.Utils
+{
+    public class PasswordNotEqualNameAttribute: ValidationAttribute
+    {
+        public PasswordNotEqualNameAttribute()
+        {
+            ErrorMessage = "Пароль не должен соответстовать имени, сука!";
+        }
+        public override bool IsValid(object value)
+        {
+            Person p = value as Person;
+            if (p.Password == p.Name)
+                return false;
+
+            return true;
+        }
+    }
+}
